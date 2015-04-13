36233b1e5f1f1987482e9a50cc727335f5fd1804
sergeymaas <chaliysergey@ya.ru>
2015-04-13 18:31:32 +0300

    new pro

---
+++ assets/js/1.js
@@ -0,0 +1,5 @@
+console.log("hello".length);
+
+var hello = function (va1) {
+    this.name = va1;
+};
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    rename = require('gulp-rename'),
+    livereload = require('gulp-livereload');
+
+gulp.task('connect', function() {
+    connect.server({
+        root: 'public',
+        livereload: true
+    });
+});
+
+
+gulp.task('js', function() {
+    gulp.src('assets/**/*.js')
+        .pipe(rename('script.js'))
+        .pipe(gulp.dest('public/js'))
+        .on('error', function(err, e) {
+            console.log(err.message);
+        })
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+    gulp.watch('assets/**/*.js', ['js']);
+});
+
+gulp.task('default', ['connect', 'js', 'watch']);
